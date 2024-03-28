eb5e8c8d7c4ca6e43a5b177582d6ad77397f7526
alex <alex@emailhiddenforever.com>
2024-03-28 13:16:06 +0000

    added walrus examples

--- Core Concepts/walrus_operator_example.py
+++ Core Concepts/walrus_operator_example.py
@@ -24,3 +24,38 @@ def get_text_info(text: str) -> dict:
 
 print(get_text_info("Andrew"))
 print(get_text_info("Andrew is here"))
+
+#example 2 - generate random number and check if its even
+
+import random
+def generate_number():
+    # Imagine this is a more complex operation
+    return random.randint(1, 100)  # Generates a random number between 1 and 100
+
+numbers = []
+for _ in range(10):  # Still wanting to check 10 numbers
+    if (num := generate_number()) % 2 == 0:
+        numbers.append(num)
+print(*numbers)
+
+#example 3 - read a file
+
+#with open('file.txt', 'r') as file:
+#    while (line := file.readline()):
+#        print(line.strip())
+
+#example 4
+
+word = "test"
+for char in word:
+    if (upper_char := char.upper()) == char:
+        print(f"{char} is uppercase.")
+    else:
+        print(f"{char} is not uppercase.")
+
+word = "test"
+for char in word:
+    if (current_char := char.lower()) == 't':
+        print(f"{char} is a 't'.")
+    else:
+        print(f"{char} is not a 't'.")
