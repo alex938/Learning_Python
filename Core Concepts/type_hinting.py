73430dde02e69b38e35b9799984ae39c0edba2ef
Alex <37157369+alex938@users.noreply.github.com>
2024-06-18 10:51:26 +0100

    Update type_hinting.py

--- Core Concepts/type_hinting.py
+++ Core Concepts/type_hinting.py
@@ -2,9 +2,25 @@ number: int = 10
 word: str = "A word"
 floating_number: float = 10.2
 
+
+
 def to_upper(elements: list[str]) -> list[str]:
     return [element.upper() for element in elements]
 
 list1: list[str] = to_upper(["John", "Bret", "Jessica"])
 
-print(list1)
+
+
+def myfunc1():
+    """myfunc1 has no parameters
+    and prints 'Hello'."""
+    print("Hello")
+
+print(help(myfunc1))
+
+Output:
+    Help on function myfunc1 in module __main__:
+    
+    myfunc1()
+        myfunc1 has no parameters
+        and prints 'Hello'.
