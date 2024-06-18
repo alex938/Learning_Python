d60df01c86e17cf5fe1a7451fb17db4bba893f5e
Alex <37157369+alex938@users.noreply.github.com>
2024-06-18 10:55:41 +0100

    Update type_hinting.py

--- Core Concepts/type_hinting.py
+++ Core Concepts/type_hinting.py
@@ -2,6 +2,10 @@ number: int = 10
 word: str = "A word"
 floating_number: float = 10.2
 
+user_name: str = "Ada"
+is_valid: bool = False
+average_time: float = 12.5
+
 
 
 def to_upper(elements: list[str]) -> list[str]:
@@ -11,6 +15,13 @@ list1: list[str] = to_upper(["John", "Bret", "Jessica"])
 
 
 
+from typing import List, Tuple, Dict
+user_names: List[str] = ["Ada", "Charles", "Alan"]
+results: Tuple[bool] = True, False, True
+config: Dict[str, int] = {"max": 100, "min": 1
+
+
+                        
 def myfunc1():
     """myfunc1 has no parameters
     and prints 'Hello'."""
