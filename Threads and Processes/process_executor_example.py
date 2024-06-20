d2e40034c0295e759131e9af5433acac5f3fca85
Alex <37157369+alex938@users.noreply.github.com>
2024-06-20 13:42:26 +0100

    Update process_executor_example.py

--- Threads and Processes/process_executor_example.py
+++ Threads and Processes/process_executor_example.py
@@ -12,3 +12,24 @@ def main():
     
 if __name__ == "__main__":
     print(main())
+
+######################################################################
+
+from concurrent.futures import ProcessPoolExecutor
+from os import getpid
+
+def calc_cube(value):
+    '''calculate a cube'''
+    print(f'executing PID {getpid()}')
+    return (value, value ** 3)
+
+def main():
+    my_ppe = ProcessPoolExecutor(max_workers=4)
+    try:
+        results = list(my_ppe.map(calc_cube, range(10)))
+    finally:
+        my_ppe.shutdown()
+    return results
+    
+if __name__ == "__main__":
+    print(main())
