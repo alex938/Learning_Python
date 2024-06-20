717a0105f71e5e8fd854e26b29e3fbf6543775b4
Alex <37157369+alex938@users.noreply.github.com>
2024-06-20 13:49:49 +0100

    Update test_palindrome.py

--- Advanced Concepts/test_palindrome.py
+++ Advanced Concepts/test_palindrome.py
@@ -23,3 +23,23 @@ def test_simple_valid_wrong_case_palindrome():
 def test_complex_palindrome_clean():
     awkwardPal = Palindrome("R*a$1ce21122121car1221%$£!")
     assert awkwardPal.is_palindrome() == True
+
+
+'''
+pip install pytest
+
+PS C:\Users\Admin\Desktop> pytest -v
+================================================================================= test session starts ================================================================================== 
+platform win32 -- Python 3.12.1, pytest-8.2.2, pluggy-1.5.0 -- C:\Users\Admin\AppData\Local\Programs\Python\Python312\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Users\Admin\Desktop
+collected 5 items
+
+test_palindrome.py::test_simple_valid_palindrome PASSED                                                                                                                           [ 20%] 
+test_palindrome.py::test_return_type_valid PASSED                                                                                                                                 [ 40%] 
+test_palindrome.py::test_simple_invalid_raises_exception_on_notstring PASSED                                                                                                      [ 60%] 
+test_palindrome.py::test_simple_valid_wrong_case_palindrome PASSED                                                                                                                [ 80%] 
+test_palindrome.py::test_complex_palindrome_clean PASSED                                                                                                                          [100%] 
+
+================================================================================== 5 passed in 0.04s ===================================================================================
+'''
